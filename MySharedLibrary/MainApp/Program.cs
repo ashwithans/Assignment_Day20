1b7386f070f17604f0baf4eb916c1a55350a15f9
ashwithans <ashwithans@gmail.com>
2023-12-01 15:35:23 +0530

    Assignment_Day20

---
+++ MySharedLibrary/MainApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySharedLibrary;
+
+namespace MainApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string userEmail = "example@example.com";
+            DataValidator validator = new DataValidator();
+
+            bool isValid = validator.IsValidEmail(userEmail);
+
+            if (isValid)
+            {
+                Console.WriteLine("Valid email address!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email address!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
